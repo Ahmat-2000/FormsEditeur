f71c2df9a72513163493064035715600cb66e851
Ahmat <63438195+Ahmat-2000@users.noreply.github.com>
2024-02-14 19:04:22 +0100

    Update Main.java

--- src/view/Main.java
+++ src/view/Main.java
@@ -14,7 +14,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("set up is done succefully Memar");
+        System..println("set up is done succefully Memar");
     }
     
 }
